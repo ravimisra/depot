c1a7a92597e9a54e6c6099055e9bbb07568799c5
Ravi Misra (LSA-LIN) <ravi@ravi-Vostro-1015>
2012-06-08 16:59:35 +0530

    Iteration: J4

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -55,7 +55,7 @@ class OrdersController < ApplicationController
 		Cart.destroy(session[:cart_id])
 		session[:cart_id] = nil
 		OrderNotifier.received(@order).deliver
-		format.html { redirect_to store_url, notice:'Thank you for your order.' }
+		format.html { redirect_to store_url, notice: t('.thanks') }
 		format.json { render json: @order, status: :created, location: @order }
 	else
 		@cart = current_cart
--- app/controllers/users_controller.rb~
+++ app/controllers/users_controller.rb~
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = User.order(:name)
 
     respond_to do |format|
       format.html # index.html.erb
