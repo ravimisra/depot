ae85264a0ae679fb831e18998ee09f6bae28d6e6
Ravi Misra (LSA-LIN) <ravi@ravi-Vostro-1015>
2012-06-12 12:55:09 +0530

    some cleanup

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,7 +5,7 @@ class OrdersController < ApplicationController
   # GET /orders.json
   def index
     @orders = Order.all
-	@orders = Order.paginate :page => params[:page], :order => 'created_at desc', :per_page => 10
+	  @orders = Order.paginate :page => params[:page], :order => 'created_at desc', :per_page => 10
 
     respond_to do |format|
       format.html # index.html.erb
